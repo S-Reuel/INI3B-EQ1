40e6f67b29126db1574453555abdb912989bfc64
Davi Rabelo Anselmo <ra2357008>
2025-08-27 11:34:25 -0300

    styles(): Mudanças no css de sprints O css e html de Sprints.jsx foi alterado para corresponder ao design. Também foi corrigido um erro em que Editar_Sprints.jsx se mostrava com o texto 'Criar Sprint'

--- projeto-rjm/src/data/services/API.jsx
+++ projeto-rjm/src/data/services/API.jsx
@@ -6,7 +6,7 @@ axios.defaults.headers.common['Authorization'] = localStorage.getItem('authToken
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = true
 const URL = axios.create({
     // baseURL: 'http://localhost:3000/api/v2/' /* Local */
-    baseURL: 'https://7c9286191455.ngrok-free.app/api/v2/'  /* Ngrok */
+    baseURL: 'https://5b9257d0b811.ngrok-free.app/api/v2/'  /* Ngrok */
 })
 
 /* Função para tratar Promise */
