258d6b9915fc9068612433ae5f776bf74e81d90c
SilasReuel <silas.reuel@unesp.br>
2025-08-27 23:02:30 -0300

    refactor: Remove campo 'data_criacao'
    Campo 'data_criacao' na tabela 'projetos' não é utilizada em nenhum momento

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_28_014522) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_28_020008) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -84,7 +84,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_28_014522) do
   create_table "projetos", force: :cascade do |t|
     t.string "nome"
     t.text "descricao"
-    t.date "data_criacao"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
