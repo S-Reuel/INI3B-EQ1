c7b3df78aefd37348a86111c17594e0a65fd2013
SilasReuel <silas.reuel@unesp.br>
2025-09-02 21:36:53 -0300

    feat(git_hub): adiciona função de payload GitHub Adiciona a função que irá armazenar os dados do payload do GitHub, havia sido criado anteriormente porém, por um erro do git, essa feat foi perdida.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   enum :status, { pendente: 0, concluido: 1, em_andamento: 2, atrasado: 3, cancelado: 4 }
 
-  has_and_belongs_to_many :git_hub
+  has_and_belongs_to_many :git_hubs, join_table: :git_hubs_tasks
   has_many :sprint_tasks
   has_many :sprints, through: :sprint_tasks
   has_many_attached :arquivos
