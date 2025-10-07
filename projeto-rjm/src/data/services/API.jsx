8e929a47a2a4ec1bf16d56252159f0ad6ce68661
Davi Rabelo Anselmo <ra2357008>
2025-10-07 12:00:13 -0300

    style(): alterações no css concertado um erro em App.jsx em relação a 'null', retirada a scrollbar dos modais, adicionadas navBars a todas as paginas (task precisa de concerto, Luigy).

--- projeto-rjm/src/data/services/API.jsx
+++ projeto-rjm/src/data/services/API.jsx
@@ -5,7 +5,7 @@ import { redirecionar, voltar } from "../../pages/util/functions"
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('authToken')
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = true
 const URL = axios.create({
-    baseURL: 'http://localhost:3000/api/v2/' /* Servidor CTI */
+    baseURL: 'http://eq1.ini3b.projetoscti.com.br/api/v2/' /* Servidor CTI */
 
 })
 
